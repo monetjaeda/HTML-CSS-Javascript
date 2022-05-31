cb025ab9fac823f98118879c7bfe1d006c3893c0
monetjaeda <105211139+monetjaeda@users.noreply.github.com>
2022-05-31 01:14:49 +0100

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,8 @@
+console.log("Monet");
+console.log("Powell");
+console.log("London");
+console.log("Capricorn");
+let mycar ="Mercedes-Benz";
+let myModel = "GLA"; 
+console.log("my fav car is:" +mycar + "and the model is:" +myModel);
+console.log("I love Javascript.");
